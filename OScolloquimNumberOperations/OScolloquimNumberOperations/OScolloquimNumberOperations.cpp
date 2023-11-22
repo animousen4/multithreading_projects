51dac78f45190bb225ec4bf59759f68d62afefa1
animousen4 <ilya_kozyro@mail.ru>
2023-11-23 00:02:00 +0300

    project creation, base unit impl

---
+++ OScolloquimNumberOperations/OScolloquimNumberOperations/OScolloquimNumberOperations.cpp
@@ -0,0 +1,12 @@
+﻿// OScolloquimNumberOperations.cpp : Defines the entry point for the application.
+//
+
+#include "OScolloquimNumberOperations.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
